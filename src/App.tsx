c5f7184f84ab32eccb1b9a87aeb1bb58493d1263
Rowan Wood <mrdiamonddirt@gmail.com>
2026-05-06 00:45:49 +0100

    feat: replace IonReactRouter with IonReactHashRouter for improved routing

--- src/App.tsx
+++ src/App.tsx
@@ -109,7 +109,7 @@ const App: React.FC = () => {
             <Redirect to="/editor" />
           </Route>
         </IonRouterOutlet>
-      </IonReactRouter>
+      </IonReactHashRouter>
     </IonApp>
   );
 };
